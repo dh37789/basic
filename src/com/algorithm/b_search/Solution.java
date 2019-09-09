704d5db9e45bd8920ce0d490426865a6b1f5d40e
dhaudgkr <dhaudgkr@gmail.com>
2019-09-09 23:36:22 +0900

    add programmers

--- src/com/algorithm/b_search/Solution.java
+++ src/com/algorithm/b_search/Solution.java
@@ -1,6 +1,17 @@
 package com.algorithm.b_search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
+
+  public static void main(String[] args) {
+    int arr[] = {5, 9, 7, 10};
+    int divisor = 52;
+
+    System.out.println(Arrays.toString(solution(arr, divisor)));
+  }
 
 
 }
