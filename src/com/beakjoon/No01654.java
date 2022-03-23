16f8a227a7efedcc959d89bb1fd9980f386dd438
dhaud <dhaudgkr@gmail.com>
2022-03-24 01:10:29 +0900

    백준 문제 추가

---
+++ src/com/beakjoon/No01654.java
@@ -0,0 +1,58 @@
+package com.beakjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class No01654 {
+    static int n;
+    static int requireLan;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer sToken = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(sToken.nextToken());
+        requireLan = Integer.parseInt(sToken.nextToken());
+
+        Long[] lanArr = new Long[n];
+
+        for (int i = 0; i < n; i++) {
+            lanArr[i] = Long.parseLong(br.readLine());
+        }
+
+        Arrays.sort(lanArr, Collections.reverseOrder());
+
+        long result = binary_search(lanArr);
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+
+    private static long binary_search(Long[] lanArr){
+        long max = lanArr[0];
+        long min  = 1;
+        long middle;
+
+        while(max >= min){
+            int lanCount = 0;
+            middle = (max + min)/2;
+
+            for (int i = 0; i < n; i++){
+                lanCount += (int) (lanArr[i]/middle);
+            }
+
+            if (lanCount < requireLan) {
+                max = middle - 1;
+            } else if (lanCount >= requireLan) {
+                min = middle + 1;
+            }
+        }
+
+        return max;
+    }
+}
