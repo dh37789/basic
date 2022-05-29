c53fc4835b624de8583628d2653b65523a3caf3a
dhaud <dhaudgkr@gmail.com>
2022-05-30 00:35:05 +0900

    Feat: add junit5 test module

---
+++ junit5/src/test/java/com/dhaudgkr/junit5/Junit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhaudgkr.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
