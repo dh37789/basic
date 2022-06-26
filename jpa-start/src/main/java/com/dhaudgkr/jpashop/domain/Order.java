51fb39d08e98ef0d84f8604cb3176fb874ece971
dhaud <dhaudgkr@gmail.com>
2022-06-26 22:22:09 +0900

    Feat: 양방향 연관관계 예시 추가

--- jpa-start/src/main/java/com/dhaudgkr/jpashop/domain/Order.java
+++ jpa-start/src/main/java/com/dhaudgkr/jpashop/domain/Order.java
@@ -15,8 +15,11 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name= "MEMBER_ID")
+    private Member member;
 
     private LocalDateTime orderDate;
 
