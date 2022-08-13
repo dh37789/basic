0a3b64a28cf57babff2f06eeb8002338f96ec1bd
dhaud <dhaudgkr@gmail.com>
2022-08-13 11:09:07 +0900

    Feat: jpql 페이징 예제

--- jpa-start/src/main/java/com/dhaudgkr/jpa07/jpql02/JpaMain.java
+++ jpa-start/src/main/java/com/dhaudgkr/jpa07/jpql02/JpaMain.java
@@ -18,75 +18,22 @@ public class JpaMain {
         entityTransaction.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            entityManager.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i+5);
+                entityManager.persist(member);
+            }
 
             entityManager.flush();
             entityManager.clear();
 
-//            /* JQPL 문법 */
-//            entityManager.createQuery("SELECT m FROM Member m", Member.class);
-//
-//            /* TypedQuery, Query */
-//            TypedQuery<Member> query1 = entityManager.createQuery("SELECT m FROM Member m", Member.class);
-//            TypedQuery<String> query2 = entityManager.createQuery("SELECT m.username FROM Member m", String.class);
-//            Query query3 = entityManager.createQuery("SELECT m.username, m.age FROM Member m");
-//
-//            /* 결과 조회 API */
-//            Member resultMember = query1.getSingleResult();
-//            System.out.println("resultMember = " + resultMember);
-//
-//            /* 파라미터 바인딩 */
-//            TypedQuery<Member> query = entityManager.createQuery("SELECT m FROM Member m WHERE m.username = :username", Member.class);
-//            query.setParameter("username", "member1");
-//            Member member1 = query.getSingleResult();
-//            System.out.println("member1 : " + member1.getUsername());
-//
-//            /* 프로젝션 */
-//            List<Member> members = entityManager.createQuery("SELECT m from Member m", Member.class)
-//                    .getResultList();
-//
-//            Member findMember = members.get(0);
-//            findMember.setAge(30);
-
-//            /* 프로젝션 */
-//            List<Team> teams = entityManager.createQuery("SELECT m.team from Member m", Team.class)
-//                    .getResultList();
-
-//            /* 프로젝션 */
-//            entityManager.createQuery("SELECT o.address from Order o", Address.class)
-//                    .getResultList();
-//
-//            /* 스칼라 프로젝션 */
-//            entityManager.createQuery("SELECT m.username, m.age from Member m")
-//                    .getResultList();
-
-            /* Object */
-            List objectList = entityManager.createQuery("SELECT m.username, m.age from Member m")
-                    .getResultList();
-
-            Object o = objectList.get(0);
-            Object[] objects = (Object[]) o;
-            System.out.println("objects.username : " + objects[0]);
-            System.out.println("objects.age : " + objects[1]);
-
-//            /* Object[] */
-//            List<Object[]> results = entityManager.createQuery("SELECT m.username, m.age from Member m")
-//                    .getResultList();
-//
-//            Object[] objects = results.get(0);
-//            System.out.println("objects.username : " + objects[0]);
-//            System.out.println("objects.age : " + objects[1]);
+//            jpqlBasic(entityManager);
+//            projections(entityManager);
+//            projections2(entityManager);
+//            paging(entityManager);
 
 
-//            /* new 이용 방식 */
-//            List<MemberDto> result = entityManager.createQuery("SELECT new com.dhaudgkr.jpa07.jpql02.domain.MemberDto(m.username, m.age) from Member m", MemberDto.class)
-//                    .getResultList();
-//            MemberDto memberDto = result.get(0);
-//            System.out.println("MemberDto.username : " + memberDto.getUsername());
-//            System.out.println("MemberDto.age : " + memberDto.getAge());
 
             entityTransaction.commit();
         } catch (Exception e) {
@@ -95,5 +42,85 @@ public class JpaMain {
             entityManager.clear();
         }
         entityManagerFactory.close();
+    }
+
+    private static void jpqlBasic(EntityManager entityManager) {
+        /* JQPL 문법 */
+        entityManager.createQuery("SELECT m FROM Member m", Member.class);
+
+        /* TypedQuery, Query */
+        TypedQuery<Member> query1 = entityManager.createQuery("SELECT m FROM Member m", Member.class);
+        TypedQuery<String> query2 = entityManager.createQuery("SELECT m.username FROM Member m", String.class);
+        Query query3 = entityManager.createQuery("SELECT m.username, m.age FROM Member m");
+
+        /* 결과 조회 API */
+        Member resultMember = query1.getSingleResult();
+        System.out.println("resultMember = " + resultMember);
+
+        /* 파라미터 바인딩 */
+        TypedQuery<Member> query = entityManager.createQuery("SELECT m FROM Member m WHERE m.username = :username", Member.class);
+        query.setParameter("username", "member1");
+        Member member1 = query.getSingleResult();
+        System.out.println("member1 : " + member1.getUsername());
+    }
+
+    private static void projections(EntityManager entityManager) {
+        /* 프로젝션 */
+        List<Member> members = entityManager.createQuery("SELECT m from Member m", Member.class)
+                .getResultList();
+
+        Member findMember = members.get(0);
+        findMember.setAge(30);
+
+        /* 프로젝션 */
+        List<Team> teams = entityManager.createQuery("SELECT m.team from Member m", Team.class)
+                .getResultList();
+
+        /* 프로젝션 */
+        entityManager.createQuery("SELECT o.address from Order o", Address.class)
+                .getResultList();
+
+        /* 스칼라 프로젝션 */
+        entityManager.createQuery("SELECT m.username, m.age from Member m")
+                .getResultList();
+    }
+
+    private static void projections2(EntityManager entityManager) {
+        /* Object */
+        List objectList = entityManager.createQuery("SELECT m.username, m.age from Member m")
+                .getResultList();
+
+        Object o = objectList.get(0);
+        Object[] objects = (Object[]) o;
+        System.out.println("objects.username : " + objects[0]);
+        System.out.println("objects.age : " + objects[1]);
+
+        /* Object[] */
+        List<Object[]> results = entityManager.createQuery("SELECT m.username, m.age from Member m")
+                .getResultList();
+
+        Object[] objects2 = results.get(0);
+        System.out.println("objects.username : " + objects2[0]);
+        System.out.println("objects.age : " + objects2[1]);
+
+
+        /* new 이용 방식 */
+        List<MemberDto> result = entityManager.createQuery("SELECT new com.dhaudgkr.jpa07.jpql02.domain.MemberDto(m.username, m.age) from Member m", MemberDto.class)
+                .getResultList();
+        MemberDto memberDto = result.get(0);
+        System.out.println("MemberDto.username : " + memberDto.getUsername());
+        System.out.println("MemberDto.age : " + memberDto.getAge());
+    }
+
+    private static void paging(EntityManager entityManager) {
+        List<Member> memberList = entityManager.createQuery("SELECT m FROM Member m ORDER BY m.age DESC", Member.class)
+                .setFirstResult(0)
+                .setMaxResults(10)
+                .getResultList();
+
+        System.out.println("memberList.size : " + memberList.size());
+        for (Member findMember : memberList) {
+            System.out.println("findMember.username : " + findMember);
+        }
     }
 }
