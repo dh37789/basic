377b4a34f58e3a474cff2640ba915c8e9c256f63
dhaud <dhaudgkr@gmail.com>
2022-08-20 16:08:20 +0900

    Feat: JPQL 조건식

--- jpa-start/src/main/java/com/dhaudgkr/jpa07/jpql02/JpaMain.java
+++ jpa-start/src/main/java/com/dhaudgkr/jpa07/jpql02/JpaMain.java
@@ -20,7 +20,7 @@ public class JpaMain {
             entityManager.persist(team);
 
             Member member = new Member();
-            member.setUsername("member");
+            member.setUsername(null);
             member.setAge(10);
             member.setType(MemberType.valueOf("USER"));
             member.setTeam(team);
@@ -35,23 +35,54 @@ public class JpaMain {
 //            paging(entityManager);
 //            join(entityManager);
 //            subQuery(entityManager);
+//            typeQuery(entityManager);
 
-//            String typeQuery = "select m.username, 'HELLO', 10L, TRUE from Member m";
-//            List<Object[]> result = entityManager.createQuery(typeQuery)
+//            String conditionalQuery = "select " +
+//                                        "m.username," +
+//                                        "case when m.age <= 10 then '학생요금'" +
+//                                        "     when m.age >= 60 then '경로요금'" +
+//                                        "     else '일반요금' " +
+//                                        "end " +
+//                                        "from Member m ";
+//            List<Object[]> result = entityManager.createQuery(conditionalQuery)
 //                    .getResultList();
 //
-//            for (Object[] objects : result) {
-//                System.out.println("member : " + objects[0]);
-//                System.out.println("String : " + objects[1]);
-//                System.out.println("Long : " + objects[2]);
-//                System.out.println("Boolean : " + objects[3]);
+//            for (Object[] obj : result) {
+//                System.out.println("username : " + obj[0]);
+//                System.out.println("fee : " + obj[1]);
 //            }
 
-            String typeQuery = "select m.username from Member m " +
-                    "where m.type = :userType";
-            List<Object[]> result = entityManager.createQuery(typeQuery)
-                    .setParameter("userType", MemberType.USER)
-                    .getResultList();
+//            String conditionalQuery = "select " +
+//                                        "t.name," +
+//                                        "case t.name" +
+//                                        "     when 'teamA' then '개발팀A'" +
+//                                        "     when 'teamB' then '개발팀B'" +
+//                                        "     else '개발팀C' " +
+//                                        "end " +
+//                                        "from Team t";
+//            List<Object[]> result = entityManager.createQuery(conditionalQuery)
+//                    .getResultList();
+//
+//            for (Object[] obj : result) {
+//                System.out.println("name : " + obj[0]);
+//                System.out.println("이름 : " + obj[1]);
+//            }
+
+//            String query = "select coalesce(m.username, '이름 없는 회원') as username from Member m";
+//            List<String> result = entityManager.createQuery(query, String.class)
+//                            .getResultList();
+//
+//            for (String name : result) {
+//                System.out.println("member name : " + name);
+//            }
+
+            String query2 = "select nullif(m.username, '관리자') as username from Member m";
+            List<String> result2 = entityManager.createQuery(query2, String.class)
+                            .getResultList();
+
+            for (String name : result2) {
+                System.out.println("member name : " + name);
+            }
 
             entityTransaction.commit();
         } catch (Exception e) {
@@ -167,6 +198,25 @@ public class JpaMain {
     private static void subQuery(EntityManager entityManager) {
         String subQuery = "select (select avg(m1.age) from Member m1) as avgAge from Member";
         List<Object[]> result = entityManager.createQuery(subQuery)
+                .getResultList();
+    }
+
+    private static void typeQuery(EntityManager entityManager) {
+            String typeQuery = "select m.username, 'HELLO', 10L, TRUE from Member m";
+            List<Object[]> result = entityManager.createQuery(typeQuery)
+                    .getResultList();
+
+            for (Object[] objects : result) {
+                System.out.println("member : " + objects[0]);
+                System.out.println("String : " + objects[1]);
+                System.out.println("Long : " + objects[2]);
+                System.out.println("Boolean : " + objects[3]);
+            }
+
+        String typeQuery2 = "select m.username from Member m " +
+                "where m.type = :userType";
+        List<Object[]> result2 = entityManager.createQuery(typeQuery2)
+                .setParameter("userType", MemberType.USER)
                 .getResultList();
     }
 }
