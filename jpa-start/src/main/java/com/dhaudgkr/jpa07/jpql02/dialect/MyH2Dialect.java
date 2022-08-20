bf8571dd6db22885f927f0c8dbb5af5b5fb0e0c5
dhaud <dhaudgkr@gmail.com>
2022-08-20 18:03:00 +0900

    Feat: JPQL 함수

---
+++ jpa-start/src/main/java/com/dhaudgkr/jpa07/jpql02/dialect/MyH2Dialect.java
@@ -0,0 +1,11 @@
+package com.dhaudgkr.jpa07.jpql02.dialect;
+
+import org.hibernate.dialect.H2Dialect;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.type.StandardBasicTypes;
+
+public class MyH2Dialect extends H2Dialect {
+    public MyH2Dialect() {
+        registerFunction("group_concat", new StandardSQLFunction("group_concat", StandardBasicTypes.STRING));
+    }
+}
