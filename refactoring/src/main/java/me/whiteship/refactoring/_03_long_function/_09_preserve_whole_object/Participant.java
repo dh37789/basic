96ac7c79fb796603b47c3420577573a183818e34
dhaud <dhaudgkr@gmail.com>
2022-09-28 00:02:32 +0900

    Feat: 냄새 3. 긴 함수

--- refactoring/src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/Participant.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/Participant.java
@@ -12,5 +12,10 @@ public record Participant(String username, Map<Integer, Boolean> homework) {
         this.homework.put(index, true);
     }
 
-
+    double getRate(int totalNumberOfEvents) {
+        long count = homework().values().stream()
+                .filter(v -> v == true)
+                .count();
+        return (double) (count * 100 / totalNumberOfEvents);
+    }
 }
