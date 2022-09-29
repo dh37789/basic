5f1b1bba116adea54d2f144bc5bf32f3ed67014a
dhaud <dhaudgkr@gmail.com>
2022-09-30 00:48:57 +0900

    Feat: 냄새 4. 긴 매개변수 목록

--- refactoring/src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_15_remove_flag_argument/Shipment.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_15_remove_flag_argument/Shipment.java
@@ -4,21 +4,21 @@ import java.time.LocalDate;
 
 public class Shipment {
 
-    public LocalDate deliveryDate(Order order, boolean isRush) {
-        if (isRush) {
-            int deliveryTime = switch (order.getDeliveryState()) {
-                case "WA", "CA", "OR" -> 1;
-                case "TX", "NY", "FL" -> 2;
-                default -> 3;
-            };
-            return order.getPlacedOn().plusDays(deliveryTime);
-        } else {
-            int deliveryTime = switch (order.getDeliveryState()) {
-                case "WA", "CA" -> 2;
-                case "OR", "TX", "NY" -> 3;
-                default -> 4;
-            };
-            return order.getPlacedOn().plusDays(deliveryTime);
-        }
+    public LocalDate regularDeliverDate(Order order) {
+        int deliveryTime = switch (order.getDeliveryState()) {
+            case "WA", "CA" -> 2;
+            case "OR", "TX", "NY" -> 3;
+            default -> 4;
+        };
+        return order.getPlacedOn().plusDays(deliveryTime);
+    }
+
+    public LocalDate rushDeliveryDate(Order order) {
+        int deliveryTime = switch (order.getDeliveryState()) {
+            case "WA", "CA", "OR" -> 1;
+            case "TX", "NY", "FL" -> 2;
+            default -> 3;
+        };
+        return order.getPlacedOn().plusDays(deliveryTime);
     }
 }
