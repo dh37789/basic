386cb5ce989257240e2ac42561d491794b55b473
dhaud <dhaudgkr@gmail.com>
2022-12-14 01:26:12 +0900

    Fix: 냄새 8: 산탄총 수술

--- refactoring/src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
@@ -2,21 +2,36 @@ package me.whiteship.refactoring._08_shotgun_surgery._29_inline_class;
 
 public class Shipment {
 
-    private TrackingInformation trackingInformation;
+    private String shippingCompany;
 
-    public Shipment(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
-    }
+    private String trackingNumber;
 
-    public TrackingInformation getTrackingInformation() {
-        return trackingInformation;
-    }
-
-    public void setTrackingInformation(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
+    public Shipment(String shippingCompany, String trackingNumber) {
+        this.shippingCompany = shippingCompany;
+        this.trackingNumber = trackingNumber;
     }
 
     public String getTrackingInfo() {
-        return this.trackingInformation.display();
+        return this.display();
+    }
+
+    public String display() {
+        return this.shippingCompany + ": " + this.trackingNumber;
+    }
+
+    public String getShippingCompany() {
+        return shippingCompany;
+    }
+
+    public void setShippingCompany(String shippingCompany) {
+        this.shippingCompany = shippingCompany;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
     }
 }
--- refactoring/src/test/java/me/whiteship/refactoring/_08_shotgun_surgery/_28_inline_function/RatingTest.java
+++ refactoring/src/test/java/me/whiteship/refactoring/_08_shotgun_surgery/_28_inline_function/RatingTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RatingTest {
-
     @Test
     void rating() {
         Rating rating = new Rating();
         assertEquals(2, rating.rating(new Driver(15)));
         assertEquals(1, rating.rating(new Driver(3)));
     }
-
 }
