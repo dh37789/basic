d78a312b19283513356754aa94e80b34a3baf968
dhaud <dhaudgkr@gmail.com>
2022-12-13 23:50:53 +0900

    Feat: 냄새 8: 산탄총 수술

--- refactoring/src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_27_move_field/CustomerContract.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_27_move_field/CustomerContract.java
@@ -6,7 +6,18 @@ public class CustomerContract {
 
     private LocalDate startDate;
 
-    public CustomerContract(LocalDate startDate) {
+    private double discountRate;
+
+    public CustomerContract(LocalDate startDate, double discountRate) {
         this.startDate = startDate;
+        this.discountRate = discountRate;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
     }
 }
