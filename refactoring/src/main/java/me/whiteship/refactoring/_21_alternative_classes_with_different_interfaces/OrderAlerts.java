766b87340fa4766f4429e5b9a3e43044b2a78755
dhaud <dhaudgkr@gmail.com>
2023-01-13 01:05:13 +0900

    Feat: 냄새 21

--- refactoring/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderAlerts.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderAlerts.java
@@ -2,12 +2,12 @@ package me.whiteship.refactoring._21_alternative_classes_with_different_interfac
 
 public class OrderAlerts {
 
-    private AlertService alertService;
+    private NotificationService notificationService;
     
     public void alertShipped(Order order) {
-        AlertMessage alertMessage = new AlertMessage();
-        alertMessage.setMessage(order.toString() + " is shipped");
-        alertMessage.setFor(order.getEmail());
-        alertService.add(alertMessage);
+        Notification notification = Notification.newNotification(order.toString() + " is shipped")
+                        .receiver(order.getEmail());
+        notificationService.sendNotification(notification);
     }
+
 }
--- refactoring/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderProcessor.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderProcessor.java
@@ -1,15 +1,13 @@
 package me.whiteship.refactoring._21_alternative_classes_with_different_interfaces;
 
 public class OrderProcessor {
-
-    private EmailService emailService;
+    private NotificationService notificationService;
 
     public void notifyShipping(Shipping shipping) {
-        EmailMessage emailMessage = new EmailMessage();
-        emailMessage.setTitle(shipping.getOrder() + " is shipped");
-        emailMessage.setTo(shipping.getEmail());
-        emailMessage.setFrom("no-reply@whiteship.com");
-        emailService.sendEmail(emailMessage);
+        Notification notification = Notification.newNotification(shipping.getOrder() + " is shipped")
+                        .receiver(shipping.getEmail())
+                        .sender("no-reply@whiteship.com");
+        notificationService.sendNotification(notification);
     }
 
 }
